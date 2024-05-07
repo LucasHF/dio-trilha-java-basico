f8a17a762d99236bd7972380e537101fcf87993e
Lucas Holanda <luks206@gmail.com>
2024-05-06 22:43:53 -0300

    inserindo o que foi descrito no TODO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,16 +1,28 @@
-Import java.util.Scanner;
+import java.util.Scanner;
 
 public class ContaTerminal {
     
 
     public static void main(String[] args) throws Exception {
-        /*TODO: Importar classe Scanner
-         * Exibir mensagens para usuário
-         * Obter informações do usuário: nome, conta, e agencia e saldo
-         * Exibir a mensagem padrão no final
-         */
-        
-         
+
+         Scanner entrada = new Scanner(System.in);
+         String nomeCliente, agencia;
+         int numero;
+         double saldo;
+
+         System.out.println("Olá! Para iniciar nosso atendimento, digite o seu nome completo:");
+         nomeCliente = entrada.nextLine();
+
+         System.out.println("Digite o número da sua agência");
+         agencia = entrada.nextLine();
+
+         System.out.println("Digite o número da sua conta");
+         numero = entrada.nextInt();
+
+         System.out.println("Digite o seu saldo");
+         saldo = entrada.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
 
     }
 
