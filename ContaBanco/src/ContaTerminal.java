76716e4e4ecf04af01940d9bd4c7833371c339ab
Lucas Holanda <luks206@gmail.com>
2024-05-06 22:20:49 -0300

    Commit inicial da estrutura do projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,17 @@
+Import java.util.Scanner;
+
+public class ContaTerminal {
+    
+
+    public static void main(String[] args) throws Exception {
+        /*TODO: Importar classe Scanner
+         * Exibir mensagens para usuário
+         * Obter informações do usuário: nome, conta, e agencia e saldo
+         * Exibir a mensagem padrão no final
+         */
+        
+         
+
+    }
+
+}
